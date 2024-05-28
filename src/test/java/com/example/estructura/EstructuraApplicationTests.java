d43b1bfa274ff2b8a55a42c6eae9717291282238
moevius237 <116665285+moevius237@users.noreply.github.com>
2024-05-28 14:02:36 +0200

    Initial commit

---
+++ src/test/java/com/example/estructura/EstructuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estructura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstructuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
